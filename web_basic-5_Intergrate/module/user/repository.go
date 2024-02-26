4d47b2325d03cfc08f55c4c17327b6584ea3e54c
ian60509 <ian.60509@gmail.com>
2024-02-26 15:10:15 +0800

    web5

---
+++ web_basic-5_Intergrate/module/user/repository.go
@@ -0,0 +1,11 @@
+package user
+
+import "github.com/ian60509/golang-web-exercise/web_basic-5_intergrate/model"
+
+type Repository interface {
+	GetUserList() (map[string]interface{}) ([]*model.User, error)
+	GetUser(in *model.User) (*model.User, error)
+	CreateUser(in *model.User) (*model.User, error)
+	ModifyUser(in *model.User, data map[string]interface{}) (*model.User, error)
+	DeleteUser(in *model.User) error
+}
---
+++ web_basic-5_Intergrate/module/user/repository/repository.go
@@ -0,0 +1,56 @@
+package repository
+//這裡會實作外層repository.go 定義的interface
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/ian60509/golang-web-exercise/web_basic-5_intergrate/model"
+	"github.com/ian60509/golang-web-exercise/web_basic-5_intergrate/module/user"
+)
+
+type UserRepository struct {
+	orm *gorm.DB
+}
+
+func NewUserRepository(orm *gorm.DB) user.Repository {
+	return &UserRepository{
+		orm: orm,
+	}
+}
+
+func (u *UserRepository) GetUserList(data map[string]interface{}) ([]*model.User, error) {
+	var (
+		err error
+		in  = make([]*model.User, 0)
+	)
+
+	err = u.orm.Find(&in, data).Error
+	return in, err
+}
+
+func (u *UserRepository) GetUser(in *model.User) (*model.User, error) {
+	var err error
+	err = u.orm.First(&in).Error
+	return in, err
+}
+
+func (u *UserRepository) CreateUser(in *model.User) (*model.User, error) {
+	var err error
+	err = u.orm.Create(&in).Error
+	return in, err
+}
+
+func (u *UserRepository) UpdateUser(in *model.User) (*model.User, error) {
+	var err error
+	err = u.orm.Save(&in).Error
+	return in, err
+}
+
+func (u *UserRepository) ModifyUser(in *model.User, data map[string]interface{}) (*model.User, error) {
+	var err error
+	err = u.orm.Model(&in).Updates(data).Error
+	return in, err
+}
+
+func (u *UserRepository) DeleteUser(in *model.User) error {
+	return u.orm.Delete(&in).Error
+}
