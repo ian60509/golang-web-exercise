3ca58086f5779bd3f46a85ae497faa2fc583114b
ian60509 <ian.60509@gmail.com>
2024-08-15 09:42:40 +0000

    modify web-basic-1

--- web_basic-1/auth.go
+++ web_basic-1/auth.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -11,10 +12,17 @@ type IndexData struct { //定義好 template 內所要的參數
 	Content string
 }
 
+// package 初始化時就會執行
 func init () {
 	UserData = map[string]string{
 		"test": "test",
 	}
+
+	fmt.Println("Init UserData: ")
+	for k, v := range UserData {
+		fmt.Printf("key: %s, value: %s\n", k, v)
+	}
+	fmt.Println("--------------\n\n\n")
 }
 
 func CheckUserIsExist(name string) bool {
@@ -45,13 +53,15 @@ func LoginAuth(c *gin.Context) {
 		password string
 	)
 
+
 	// -------- 確認 username 欄位有輸入 --------
 	if in, exist := c.GetPostForm("username"); exist  { //使用gin提供的表單功能
 		username = in 
 	} else { //沒有填寫 username
-		c.HTML(http.StatusBadRequest, "login.html", gin.H{
+		// 這個 gin.H 並非header，而是一個用於傳遞資料的map[string]interface{}配合gin的HTML模板引擎
+		c.HTML(http.StatusBadRequest, "login.html", gin.H{ //用於配合HTML的"模板引擎"，將error資料傳入HTML，讓 HTML 中透過 .error 捕捉到訊息 
 			"error": errors.New("必須輸入使用者名稱"),
-		})
+		}) //將錯誤訊息放在 header 回傳，這個header欄位是一個 ket-value pair => key: error, value: "必須輸入使用者名稱"
 		return
 	}
 
@@ -67,6 +77,7 @@ func LoginAuth(c *gin.Context) {
 
 	// -------- 驗證使用者名稱與密碼 --------
 	if err := Auth(username, password); err != nil {
+		// 設定登入成功的訊息，這個訊息會在HTML中的 .success 捕捉到
 		c.HTML(http.StatusBadRequest, "login.html", gin.H{
 			"success": "登入成功啦啦啦", //回傳成功訊息
 		})
--- web_basic-1/main.go
+++ web_basic-1/main.go
@@ -8,13 +8,13 @@ import (
 func main () {
 	server := gin.Default() //建立 gin instance
 	server.LoadHTMLGlob("template/html/*") //載入 templates 資料夾內的所有檔案
-	server.Static("/assets", "./template/assets") //設定靜態檔案路由
+	server.Static("/assets", "./template/assets") //設定靜態檔案路由，讓HTML可以直接透過 "/assets" 引用 "./template/assets" 資料夾內的檔案
 	server.GET("/login", LoginPage) //將/login這個path 綁訂到LoginPage這個function
 	server.POST("/login", LoginAuth) //將/login這個path(如果面臨 http POST request) 綁訂到LoginAuth這個function
-	server.Run(":8888") //啟動 server (預設 port 為 8080
+	server.Run(":8080") //啟動 server (預設 port 為 8080
 }
 
 func LoginPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "login.html", nil) //顯示登入頁面
+	c.HTML(http.StatusOK, "login.html", nil) //顯示登入頁面的HTML，因為前面有透過 LoadHTMLGlob 載入 templates 資料夾內的所有檔案，所以這邊可以直接寫檔名
 }
 
