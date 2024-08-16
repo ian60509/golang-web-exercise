cfc7e33dd9fe638b6330c8d7242ebb6b978af188
ian60509 <ian.60509@gmail.com>
2024-08-16 06:01:44 +0000

    update web-basic-3-ORM

--- web_basic-3_ORM/README.md
+++ web_basic-3_ORM/README.md
@@ -1,3 +1,7 @@
+
+
+
+
 執行 go run main.go
 
 結果:
@@ -6,3 +10,7 @@ Migrate success
 Create user success
 Query user success, user:&main.User{ID:11, Username:"test", Password:"test_pwd"}
 即為正確
+
+---
+### 參考連結
+https://ithelp.ithome.com.tw/articles/10234820
--- web_basic-3_ORM/main.go
+++ web_basic-3_ORM/main.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/mysql" // gorm 提供的 mysql driver
 	"gorm.io/gorm"
 	"fmt"
 )
   
-  const ( //database connection info 
-	USERNAME = "root"
-	PASSWORD = "22387681"
+const ( //database connection info 
+	USERNAME = "demo"
+	PASSWORD = "demo123"
 	NETWORK = "tcp"
-	SERVER = "127.0.0.2"
+	SERVER = "127.0.0.1"
 	PORT = 3306
 	DATABASE = "demo"
 )
 func main() {
 	dsn := fmt.Sprintf("%s:%s@%s(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",USERNAME,PASSWORD,NETWORK,SERVER,PORT,DATABASE)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{}) //開啟資料庫連線 
 	if err != nil {
-		panic("use gorm connection MySQL failed, err:" + err.Error())
+		panic("use gorm connection MySQL failed, err:" + err.Error()) //如果錯誤引發panic
 	}
+
 	fmt.Println("connect MySQL success")
 
-	if err := db.AutoMigrate(&User{}); err != nil {
+	if err := db.AutoMigrate(&User{}); err != nil { //會根據 User struct 來建立 table(或是比較現有的table 和 struct 的差異) => 如果有差異就會更新table
 		panic("Migrate failure, err:" + err.Error())
 	}
 	fmt.Println("Migrate success")
@@ -44,21 +45,48 @@ func main() {
 		panic("Query user failure, err:" + err.Error())
 	}
 
+	//-----------------更新資料-----------------
+	user.Password = "test_pwd_update"
+	if err := UpdateUser(db, user); err != nil {
+		panic("Update user failure, err:" + err.Error())
+	} else {
+		user, _ = FindUser(db, user.ID)
+		fmt.Println("Update user success: ", user)
+	}
+
+	//-----------------刪除資料-----------------
+	if err := DeleteUser(db, user); err != nil {
+		panic("Delete user failure, err:" + err.Error())
+	} else {
+		fmt.Println("Delete user success")
+	}
+
 }
 
-type User struct {
+type User struct { // 用來對應到資料庫中的table的物件
 	ID int64 `json:"id" gorm:"primary_key;auto_increase"`
 	Username string `json:"username"`
 	Password string `json:"password"`
+	Height int `json:"height"`
 }
 
 func CreateUser(db *gorm.DB, user *User) error {
+	// 只需要 user 欄位和資料庫中的欄位對應即可
 	return db.Create(user).Error //在這個database 新增一筆user 資料
+	// 如果 Create 過程中有錯誤，會設置到 Error 屬性中
 }
 
 func FindUser(db *gorm.DB, id int64) (user *User, err error) { //回傳從資料庫中找到的這個物件
 	user = new(User)
 	user.ID = id
-	err = db.First(user).Error //尋找第一筆符合的資料
+	err = db.First(user).Error //將這個物件直接丟給 gorm 幫忙找 => 尋找第一筆符合的資料
 	return user, err
+}
+
+func UpdateUser(db *gorm.DB, user *User) error {
+	return db.Save(user).Error //更新資料
+}
+
+func DeleteUser(db *gorm.DB, user *User) error {
+	return db.Delete(user).Error //刪除資料
 }
